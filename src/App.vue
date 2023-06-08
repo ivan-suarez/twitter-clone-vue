e26377ef2c4c786f8d2849f791814ad8c09195c6
ivan-suarez <ivansuarezcast@gmail.com>
2023-06-08 19:18:58 +0200

    Implement basic html structure

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<template id="template">
+  <div id="tmp">
+    <div id="nav-bar">
+    <div id="logo"></div>
+    <div>Home</div>
+    <div>Explore</div>
+    <div>Notifications</div>
+    <div>Messages</div>
+    <div>Lists</div>
+    <div>Bookmarks</div>
+    <div>Verified</div>
+    <div>Profile</div>
+    <div>Options</div>
+    <button>Tweet</button>
+    <div>
+      <div id="profile-picture"></div>
+      <div>Elon Musk</div>
+      <div>@elonmusk</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+  <div id="content">
+    <div>Home</div>
+    <div>What is happening?!</div>
+    <div id="tweet">
+      <div>SpaceX</div>
+      <div>Liftoff!</div>
+      <div id="media"></div>
+    </div>
+  </div>
+  <div id="ads">
+    <div>Search Twitter</div>
+    <div>What's happening</div>
+    <div>Who to follow</div>
+  </div>
+  </div>
+  
+  
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+#tmp{
+  display: flex;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
