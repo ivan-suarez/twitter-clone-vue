7e686e40254d2130d0f7a88580d790401de12ecc
ivan-suarez <ivansuarezcast@gmail.com>
2023-06-09 11:41:29 +0200

    Implement basic layout

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,33 @@
 
 <template id="template">
   <div id="tmp">
-    <div id="nav-bar">
-    <div id="logo"></div>
-    <div>Home</div>
-    <div>Explore</div>
-    <div>Notifications</div>
-    <div>Messages</div>
-    <div>Lists</div>
-    <div>Bookmarks</div>
-    <div>Verified</div>
-    <div>Profile</div>
-    <div>Options</div>
-    <button>Tweet</button>
-    <div>
-      <div id="profile-picture"></div>
-      <div>Elon Musk</div>
-      <div>@elonmusk</div>
+    <div id="left">
+      <div id="nav-bar">
+        <div id="logo"></div>
+        <div>Home</div>
+        <div>Explore</div>
+        <div>Notifications</div>
+        <div>Messages</div>
+        <div>Lists</div>
+        <div>Bookmarks</div>
+        <div>Verified</div>
+        <div>Profile</div>
+        <div>Options</div>
+        <button>Tweet</button>
+      
     </div>
-  </div>
+  <div id="profile-menu">
+        <div id="profile-picture"></div>
+        <div>Elon Musk</div>
+        <div>@elonmusk</div>
+      </div>
+    </div>
+    
   <div id="content">
     <div>Home</div>
-    <div>What is happening?!</div>
+    <div>What is happening?!
+      <button>Tweet</button>
+    </div>
     <div id="tweet">
       <div>SpaceX</div>
       <div>Liftoff!</div>
@@ -42,6 +48,40 @@
 <style>
 #tmp{
   display: flex;
+}
+
+#content{
+  border-style: solid;
+  border-color: lightgray;
+  flex: 1;
+  min-height: 100vh;
+}
+
+#tweet{
+  border-style: solid;
+  border-color: lightgray;
+}
+
+#left{
+  display: flex;
+  flex: 1;
+  min-height: 100vh;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+#nav-bar{
+  display: flex;
+  flex-direction: column;
+  
+}
+
+#ads{
+  flex: 1;
+}
+#profile-menu{
+  justify-content: flex-end;
 }
 
 </style>
